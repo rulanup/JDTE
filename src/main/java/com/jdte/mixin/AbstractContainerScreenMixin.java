6e73ee56b51512356060bf241f0053a4cee0c552
guili <dnsdkdbbdb@gmail.com>
2026-05-25 09:50:59 +0800

    fix: Fix Mixin inheritance for font field
    - Create ScreenMixin for font field access
    - Update AbstractContainerScreenMixin to extend ScreenMixin
    - Add getGuiLeft/getGuiTop shadows

--- src/main/java/com/jdte/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/com/jdte/mixin/AbstractContainerScreenMixin.java
@@ -1,14 +1,15 @@
 package com.jdte.mixin;
 
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(AbstractContainerScreen.class)
-public abstract class AbstractContainerScreenMixin {
+public abstract class AbstractContainerScreenMixin extends ScreenMixin {
     @Shadow protected int imageWidth;
-    @Shadow protected Font font;
+    @Shadow protected int imageHeight;
 
     @Shadow public abstract int getGuiLeft();
     @Shadow public abstract int getGuiTop();
---
+++ src/main/java/com/jdte/mixin/ScreenMixin.java
@@ -0,0 +1,11 @@
+package com.jdte.mixin;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Screen.class)
+public abstract class ScreenMixin {
+    @Shadow protected Font font;
+}
