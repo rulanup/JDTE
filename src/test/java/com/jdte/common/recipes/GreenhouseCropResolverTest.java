c4626efcd7c0c66db00a10873c336a58432436b1
guili <dnsdkdbbdb@gmail.com>
2026-08-26 11:37:08 +0800

    test: add compatibility regression coverage

---
+++ src/test/java/com/jdte/common/recipes/GreenhouseCropResolverTest.java
@@ -0,0 +1,21 @@
+package com.jdte.common.recipes;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class GreenhouseCropResolverTest {
+
+    @Test
+    void genericFallbackResolvesOrdinaryCropWithoutLevel() {
+        GreenhouseCropDefinition definition = GreenhouseCropResolver.findGeneric(
+                new ItemStack(Items.WHEAT_SEEDS));
+
+        assertNotNull(definition);
+        assertEquals(Items.WHEAT, definition.outputs().getFirst().getItem());
+        assertEquals("minecraft:wheat", definition.displayBlock().toString());
+    }
+}
---
+++ src/test/java/com/jdte/common/utils/SpawnEggEntityDataTest.java
@@ -0,0 +1,44 @@
+package com.jdte.common.utils;
+
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SpawnEggEntityDataTest {
+
+    @Test
+    void copiesEntityDataWithoutSharingMutableTag() {
+        ItemStack stack = new ItemStack(new Item(new Item.Properties()));
+        CompoundTag source = new CompoundTag();
+        source.putString("id", "productivebees:configurable_bee");
+        source.putString("type", "productivebees:life_fluid");
+        stack.set(DataComponents.ENTITY_DATA, CustomData.of(source));
+
+        Optional<CompoundTag> copy = SpawnEggEntityData.copy(stack);
+
+        assertTrue(copy.isPresent());
+        assertNotSame(source, copy.get());
+        assertEquals("productivebees:life_fluid", copy.get().getString("type"));
+        copy.get().putString("type", "minecraft:bee");
+        assertEquals("productivebees:life_fluid", stack.get(DataComponents.ENTITY_DATA).copyTag()
+                .getString("type"));
+    }
+
+    @Test
+    void missingEntityDataIsEmpty() {
+        Optional<CompoundTag> copy = SpawnEggEntityData.copy(
+                new ItemStack(new Item(new Item.Properties())));
+
+        assertFalse(copy.isPresent());
+    }
+}
